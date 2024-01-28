439c11a83bd8cc0409395138f98cd91512b1a797
Tayba <tayba.jusab25@gmail.com>
2024-01-28 16:48:40 -0500

    Remove test string

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,7 +10,6 @@ import { fadeIn, textVariant } from '../utils/motion';
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      test
       <Tilt
         options={{
           max: 45,
