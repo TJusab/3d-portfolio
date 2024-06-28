e07201d3fac8a09c9371d86660315cccfbb83906
Tayba <tayba.jusab25@gmail.com>
2024-06-28 17:07:23 -0400

    Add typewriter effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Tilt from 'react-parallax-tilt';
-import { motion } from 'framer-motion';
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -18,18 +18,20 @@ const ServiceCard = ({ index, title, icon }) => {
           options={{
             max: 45,
             scale: 1,
-            speed: 450
+            speed: 450,
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
           flex justify-evenly items-center flex-col"
         >
-          <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
-  )
-}
+  );
+};
 
 const About = () => {
   return (
@@ -38,24 +40,26 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-justify"
       >
-        I am a curious and enthusiastic software engineering student at McGill University. I especially love
-        to create innovative and captivating user experiences in web development. I also have a strong interest in
-        ML and AI. Other than coding, I enjoy creating 3D art and combining my 3D models with my coding projects is my new
-        favorite thing to do!
+        I am a curious and enthusiastic software engineering student at McGill
+        University. I especially love to create innovative and captivating user
+        experiences in web development. I also have a strong interest in mobile
+        development and ML. Other than coding, I enjoy creating 3D art and
+        combining my 3D models with my coding projects is my new favorite thing
+        to do!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service}/>
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
-
       </div>
     </>
-  )
-}
+  );
+};
 
 export default SectionWrapper(About, "about");
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,58 @@
-import { styles } from '../styles';
-
+import { styles } from "../styles";
+import TypeWriter from "typewriter-effect";
 
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.padding} absolute inset-0 top-[120px] max-w-7xl mx-auto justify-center`}>
+      <div
+        className={`${styles.padding} absolute inset-0 top-[120px] max-w-7xl mx-auto justify-center`}
+      >
         <div className="flex flex-col items-center">
-          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className="text-secondary">Tayba</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am a Software Engineering Student at McGill.</p>
+          <div className="flex flex-row">
+            <div className={`${styles.heroHeadText}`}>
+              <TypeWriter
+                options={{ cursor: "" }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Hi, I'm Tayba !")
+                    .changeDeleteSpeed(500)
+                    .deleteChars(1)
+                    .typeString(" :)")
+                    .pauseFor(500)
+                    .changeDeleteSpeed(50)
+                    .deleteChars(9)
+                    .start();
+                }}
+              />
+            </div>
+            <div className={`${styles.heroHeadText} text-secondary ml-5`}>
+              <TypeWriter
+                options={{ cursor: "" }}
+                onInit={(typewriter) => {
+                  typewriter.pauseFor(6000).typeString(" Tayba").start();
+                }}
+              />
+            </div>
+          </div>
+          <div className={`${styles.heroSubText} text-white`}>
+            <TypeWriter
+              options={{ cursor: "" }}
+              onInit={(typewriter) => {
+                typewriter
+                  .pauseFor(7000)
+                  .changeDelay(50)
+                  .typeString("I am a 2nd Year")
+                  .pauseFor(500)
+                  .deleteChars(8)
+                  .typeString("3rd Year Software Engineering Student.")
+                  .start();
+              }}
+            />
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
